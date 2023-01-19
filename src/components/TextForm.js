9c017013344b7346d07075a59b8867d5cd79272e
hassamarif <arifhassam18@gmail.com>
2023-01-20 04:36:17 +0500

    Updates Pushed

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -11,21 +11,30 @@ export default function TextForm(props) {
   };
 
   const [text, setText] = useState("Enter Text Here");
-  //   if you want to change the text
-  // setText("new Text");
   return (
-    <div>
-      <h1>{props.heading}</h1>
-      <textarea
-        className="form-control"
-        value={text}
-        onChange={handleOnChange}
-        id="exampleFormControlTextarea1"
-        rows="8"
-      ></textarea>
-      <button className="btn btn-primary m-2" onClick={handleUpClick}>
-        Convert to UperCase
-      </button>
-    </div>
+    <>
+      <div>
+        <h1>{props.heading}</h1>
+        <textarea
+          className="form-control"
+          value={text}
+          onChange={handleOnChange}
+          id="exampleFormControlTextarea1"
+          rows="8"
+        ></textarea>
+        <button className="btn btn-primary m-2" onClick={handleUpClick}>
+          Convert to UperCase
+        </button>
+      </div>
+      <div className="container">
+        <h1>Your Text Summry</h1>
+        <p>
+          Words {text.split(" ").length} and chracters {text.length}
+        </p>
+        <p>Minutes Read {0.008 * text.split(" ").length}</p>
+        <h1>Preview</h1>
+        <p>{text}</p>
+      </div>
+    </>
   );
 }
